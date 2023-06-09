1185baaaf1820c2eb4a171afa9e701217284f47a
Kev1nByte <kevinmonterozapata@gmail.com>
2023-06-09 12:03:28 -0500

    Modulos Pt 1,2,3

---
+++ views/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+
+    <strong>Copyrigth &copy; 2023 <a href="kevin.byte"
+     target="_blank">Con eso te lo digo todo</a></strong>
+
+     Todos los derechos reservados.
+
+</footer>
---
+++ views/plantilla.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Inventory System</title>
+  
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <!--=====================================
+  PLUGINS DE JAVASCRIPT
+  ======================================-->
+  <!-- jQuery 3 -->
+  <script src="views/bower_components/jquery/dist/jquery.min.js"></script>
+  
+  <!-- Bootstrap 3.3.7 -->
+  <script src="views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  
+  <!-- SlimScroll -->
+  <script src="views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  
+  <!-- FastClick -->
+  <script src="views/bower_components/fastclick/lib/fastclick.js"></script>
+  
+  <!-- AdminLTE App -->
+  <script src="views/dist/js/adminlte.min.js"></script>
+  
+  <!-- AdminLTE for demo purposes -->
+  <script src="views/dist/js/demo.js"></script>
+
+  <!--=====================================
+  PLUGINS DE CSS
+  ======================================-->
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="views/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="views/bower_components/font-awesome/css/font-awesome.min.css">
+  
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="views/bower_components/Ionicons/css/ionicons.min.css">
+  
+  <!-- Theme style -->
+  <link rel="stylesheet" href="views/dist/css/AdminLTE.css">
+
+  <!-- AdminLTE Skins.-->
+  <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<!--=====================================
+  CUERPO DEL DOCUMENTO
+  ======================================-->
+<body class="hold-transition skin-blue sidebar-collapse sidebar-mini">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <!-- =============================================== -->
+
+  <!-- =============================================== -->
+
+</div>
+<!-- ./wrapper -->
+
+  <?php
+
+    /** CABEZOTE */
+
+      include "modulos/cabezote.php";
+
+    /** MENU LATERAL */
+
+      include "modulos/menu.php";
+
+    /** Contenido de la Página */
+
+        include "modulos/contenido.php";
+
+    /** Footer */
+
+    include "modulos/footer.php";
+
+  ?>
+
+<script src="views/js/plantilla.js"></script>
+
+</body>
+</html>
