f0bbd8691d5a29bca71f2ddadce8e7e2bd7d846a
Kev1nByte <kevinmonterozapata@gmail.com>
2023-06-22 15:34:37 -0500

    non-repeated user

--- ajax/usuarios.ajax.php
+++ ajax/usuarios.ajax.php
@@ -40,6 +40,21 @@ class AjaxUsuarios{
 
 	}
 
+	// VALIDAR NO REPETIR USUARIO
+
+	public $validarUsuario;
+
+	public function ajaxValidarUsuario(){
+
+		$item = "usuario";
+		$valor = $this->validarUsuario;
+
+		$respuesta = ControladorUsuarios::ctrMostrarUsuarios($item, $valor);
+
+		echo json_encode($respuesta);
+
+	}
+
 }
 
 // EDITAR USUARIO
@@ -62,5 +77,16 @@ if(isset($_POST["activarUsuario"])){
 	$activarUsuario -> ajaxActivarUsuario();
 
 }
+
+// VALIDAR NO REPETIR USUARIO
+
+if(isset( $_POST["validarUsuario"])){
+
+	$valUsuario = new AjaxUsuarios();
+	$valUsuario -> validarUsuario = $_POST["validarUsuario"];
+	$valUsuario -> ajaxValidarUsuario();
+
+}
+
 
 
